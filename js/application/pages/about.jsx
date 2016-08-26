54c02b514b3b7874894aa3f9f569e6b8fd435e8a
Kevin <kcoxe@keywcorp.com>
2016-08-25 22:04:25 -0400

    add about page

--- js/application/pages/about.jsx
+++ js/application/pages/about.jsx
@@ -1,14 +1,23 @@
 'use strict';
 
 import React from 'react';
-import { Card, CardActions, CardHeader, CardTitle, CardText } from 'material-ui/Card';
-import AppBar from 'material-ui/AppBar';
+import { CardTitle, CardText } from 'material-ui/Card';
+import Avatar from 'material-ui/Avatar';
+import Divider from 'material-ui/Divider';
+import {List, ListItem} from 'material-ui/List';
+import Subheader from 'material-ui/Subheader';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import ContentAdd from 'material-ui/svg-icons/content/add';
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
 
 import {Grid, Row, Col} from 'react-flexbox-grid';
 import myFlexStyle from '../styles/myFlexStyle.scss';
 
-import Tabed from '../components/Tabed.jsx';
 import Panel from '../components/Panel.jsx';
+
+import { contributors } from '../config/contributors.json';
 
 
 class Test extends React.Component {
@@ -18,63 +27,88 @@ class Test extends React.Component {
   }
 
 
+  _openLink(dest) {
+    let win = window.open(dest, "_blank");
+    win.focus();
+  }
+
+
   render() {
 
-    const cardList = [
-      {
-        title: "First Tab",
-        content: <div>
-            <CardHeader
-              title="header"
-              subtitle="card header"
-            />
-            <CardTitle title="Welcome!" subtitle="card title" />
-            <CardText>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-              Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-              Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-            </CardText>
-          </div>,
-      },
-      {
-        title: "Second Tab",
-        content: <div>
-            <CardHeader
-              title="Another header"
-              subtitle="card header"
-            />
-            <CardTitle title="Welcome Again!" subtitle="card title" />
-            <CardText>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-              Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-              Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-            </CardText>
-          </div>,
-      },
-    ];
+    const contributorList = contributors.map((contributor, i) => {
 
-    const content = (
-      <Tabed content={cardList}/>
-    );
+      let iconButtonElement = (
+        <IconButton
+          touch={true}
+        >
+          <MoreVertIcon />
+        </IconButton>
+      );
 
+      let rightIconMenu = (
+        <IconMenu iconButtonElement={iconButtonElement}>
+          <MenuItem
+            primaryText="Github"
+            onTouchTap={this._openLink.bind(this, contributor.github)}
+          />
+          <MenuItem
+            primaryText="Website"
+            onTouchTap={this._openLink.bind(this, contributor.url)}
+          />
+        </IconMenu>
+      );
 
+      return (
+        <ListItem
+          leftAvatar={<Avatar src="https://avatars.githubusercontent.com/u/6655527?v=3" />}
+          primaryText={contributor.name}
+          secondaryText={
+            <IconButton iconClassName="muidocs-icon-custom-github" />
+          }
+          secondaryTextLines={2}
+          rightIconButton={rightIconMenu}
+          key={i}
+        />
+      )
+    });
 
     return (
       <Grid>
         <Row>
-          <Col md={6}>
-            <Panel content={content} />
-          </Col>
-          <Col md={6}>
+
+          <Col md={12}>
             <Panel>
-              <AppBar
-                title="test"
-              />
-              <h1>This is a test</h1>
+              <Row>
+                <Col md={6}>
+                  <CardTitle
+                    title="About"
+                    subtitle="What is Scrumdiddlyumptious?"
+                  />
+                  <CardText>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+                    Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+                    Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+                  </CardText>
+                </Col>
+
+                <Col md={6}>
+                  <List>
+                    <Subheader>Contributors</Subheader>
+                    { contributorList }
+                    <ListItem
+                      leftAvatar={<ContentAdd />}
+                      primaryText="You"
+                      secondaryText={
+                        <p>You can join</p>
+                      }
+                    />
+                  </List>
+                </Col>
+              </Row>
             </Panel>
           </Col>
+
         </Row>
       </Grid>
     );
--- js/application/pages/index.jsx
+++ js/application/pages/index.jsx
@@ -1,6 +1,10 @@
 'use strict';
 
 import React from 'react';
+import { CardTitle, CardText } from 'material-ui/Card';
+
+import {Grid, Row, Col} from 'react-flexbox-grid';
+import myFlexStyle from '../styles/myFlexStyle.scss';
 
 import Panel from '../components/Panel.jsx';
 
@@ -17,9 +21,26 @@ class Index extends React.Component {
     const title = this.props.title || "Title"
 
     return (
-      <div>
-        <Panel title={title}/>
-      </div>
+      <Grid>
+        <Row>
+
+          <Col md={12}>
+            <Panel>
+              <CardTitle
+                title={title}
+                subtitle="Make it agile"
+              />
+              <CardText>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+                Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+                Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+              </CardText>
+            </Panel>
+          </Col>
+
+        </Row>
+      </Grid>
     );
   }
 }
